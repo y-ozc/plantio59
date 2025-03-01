5bc929d31ce5bdd7b0b387170b32102477e76b4f
y-ozc <y.ozcitak@student.maastrichtuniversity.nl>
2025-03-01 17:47:29 +0100

    push before render

--- server.py
+++ server.py
@@ -89,4 +89,5 @@ def add_data():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Render assigns a PORT dynamically
+    app.run(debug=False, host="0.0.0.0", port=port)
