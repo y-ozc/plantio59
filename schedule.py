0a77b23b7da30f96d9153c7d42c2520bf1585392
nssarihanmaas <163178965+nssarihanmaas@users.noreply.github.com>
2025-03-08 16:28:51 +0100

    cultivar data message added

--- schedule.py
+++ schedule.py
@@ -1,7 +1,31 @@
 import json
+import os
+import random
 
-# web app will interract with this function
-# STUB
-# exmaple function
-def scheduleFunction():
-    return "Have some tasks."
+
+def load_cultivars():
+    # Get the path to the JSON file (relative path)
+    file_path = os.path.join(os.path.dirname(__file__), 'data', 'cultivars_data.json')
+
+    # Read the JSON file and return it as a Python object (list, dictionary, etc.)
+    try:
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        return data  # Return the data loaded from the JSON file
+    except FileNotFoundError:
+        print("data\cultivars_data.json file not found")
+        return None
+    except json.JSONDecodeError:
+        print("Error decoding JSON")
+        return None
+
+
+def pick_category(user_vector):
+    # Get categories
+    selected_items = list(user_vector.items())[7:10]  # Slice from seventh to tenth entry
+
+    # Unzip keys and values
+    _, weights = zip(*selected_items)  # Ignore names, keep weights
+
+    # Pick a number from 1 to 4 using weights
+    return random.choices(range(1, 5), weights=weights)
